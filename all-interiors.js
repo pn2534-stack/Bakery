5578fb948f2edab49ed362e29846de5c0e33bf56
pn2534-stack <pn2534@nyu.edu>
2026-07-15 22:51:08 -0400

    commit

--- all-interiors.js
+++ all-interiors.js
@@ -11,7 +11,7 @@
     'cottage|Bathroom':{'Shower':[53,14,24,41],'Sink':[15,27,22,29],'Toilet':[82,42,13,28],'Towel rack':[39,31,12,24]},
     'shops|Furniture':{'Living room display':[4,24,28,27],'Bedroom display':[43,8,22,28],'Kitchen display':[67,15,22,30],'Bathroom display':[84,37,15,25],'Garden display':[0,55,25,32],'Pet furniture':[66,55,25,31]},
     'shops|Boutique':{'Women clothing':[36,34,18,26],'Men clothing':[55,35,19,25],'Shoes':[35,54,25,18],'Accessories':[8,37,20,25],'Hats':[5,29,20,22],'Jewelry':[80,43,18,24],'Changing room':[42,13,16,24],'Mirror':[22,22,11,32],'Cashier':[64,17,18,24]},
-    'park|Pavilion':{'Tea tables':[42,34,31,28],'Tea set':[48,37,12,15],'Cake stand':[63,39,10,16],'Flower garden':[72,54,22,24],'Picnic area':[82,68,16,18],'Butterfly garden':[23,40,17,24],'Duck pond':[0,58,24,32],'Mini games':[75,24,15,20],'Playground':[84,17,14,24],'Food stand':[3,26,19,29]},
+    'park|Pavilion':{'Tea tables':[35,38,22,25],'Tea set':[59,38,10,18],'Cake stand':[71,38,10,18],'Mini games':[82,22,15,22]},
     'park|Gardens':{'Flower garden':[35,31,25,26],'Butterfly garden':[60,27,22,25],'Walking trail':[37,49,27,32],'Duck pond':[0,48,28,37],'Playground':[66,7,22,27],'Picnic blanket':[67,55,21,23],'Food stand':[83,34,16,28]},
     'library|Reading Hall':{'Bookshelves':[0,11,22,38],'Fireplace':[22,21,17,31],'Reading chairs':[18,37,27,25],'Study tables':[53,28,24,25],'Recipe books':[28,51,19,25],'Magazine rack':[46,49,22,26],'Tea corner':[80,57,18,27]},
     'greenhouse|Greenhouse':{'Flower beds':[5,48,25,30],'Vegetable beds':[31,40,27,29],'Herb shelves':[81,18,17,29],'Fruit trees':[0,24,18,36],'Seed cabinet':[49,11,13,25],'Watering area':[78,38,18,27]},
